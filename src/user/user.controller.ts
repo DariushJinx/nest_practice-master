b28217cf2634ddfd5fa29e7ad6e8f500485fe739
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-04 00:46:40 +0330

    file updated with login dto and express req

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/user.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { LoginUserDto } from './dto/login.dto';
+import { ExpressRequest } from 'src/types/expressRequest.interface';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
+  @UsePipes(new ValidationPipe())
   async createUser(
     @Body() createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
     return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(@Body() loginDto: LoginUserDto): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Get('user')
+  async currentUser(
+    @Req() req: ExpressRequest,
+  ): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(req.user);
   }
 }
