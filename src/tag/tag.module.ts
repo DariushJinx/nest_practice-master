d4698cd6b2566fc4b022f7ceb53586c783fa075c
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-03 18:18:52 +0330

    handle module for tag

---
+++ src/tag/tag.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { tagController } from './tag.controller';
+import { TagService } from './tag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from './tag.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([TagEntity])],
+  controllers: [tagController],
+  providers: [TagService],
+})
+export class TagModule {}
