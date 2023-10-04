7b163d0a07a7f7cd32969698f48d0c5a15b23ec6
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-04 18:06:59 +0330

    handle update user route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,9 @@ import {
   Controller,
   Get,
   Post,
+  Put,
   Req,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -12,6 +14,10 @@ import { CreateUserDto } from './dto/user.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
 import { LoginUserDto } from './dto/login.dto';
 import { ExpressRequest } from 'src/types/expressRequest.interface';
+import { User } from './decorators/user.decorators';
+import { UserEntity } from './user.entity';
+import { AuthGuard } from './guard/auth.guard';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller()
 export class UserController {
@@ -34,9 +40,27 @@ export class UserController {
   }
 
   @Get('user')
+  // نحوه استفاده از گارد که در ابتدا
+  // میدلویر مربوط به آتورایزیشن اتفاق میفته بعدش گارد
+  @UseGuards(AuthGuard)
   async currentUser(
     @Req() req: ExpressRequest,
+    @User() user: UserEntity,
   ): Promise<UserResponseInterface> {
-    return this.userService.buildUserResponse(req.user);
+    // return this.userService.buildUserResponse(req.user);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Put('user')
+  @UseGuards(AuthGuard)
+  async updateCurrentUser(
+    @User('id') currentUserId: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.updateUser(
+      currentUserId,
+      updateUserDto,
+    );
+    return this.userService.buildUserResponse(user);
   }
 }
