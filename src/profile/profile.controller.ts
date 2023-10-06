3ce3a10ae7806f570fa684989a529e18225bedf6
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-06 21:06:34 +0330

    handle controller for profile

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { User } from 'src/user/decorators/user.decorators';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+import { AuthGuard } from 'src/user/guard/auth.guard';
+
+@Controller('profiles')
+export class ProfilesController {
+  constructor(private readonly profileService: ProfileService) {}
+
+  @Get(':username')
+  async getProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return await this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return await this.profileService.buildProfileResponse(profile);
+  }
+
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unFollowProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.unFollowProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return await this.profileService.buildProfileResponse(profile);
+  }
+}
