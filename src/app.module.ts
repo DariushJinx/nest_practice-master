c19a09fe00e5eef4aed707a54d282265b75ea0ad
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-06 21:04:02 +0330

    file updated with profile module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import ormconfig from './ormconfig';
 import { UserModule } from './user/user.module';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
 import { ArticleModule } from './article/article.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ArticleModule } from './article/article.module';
     TagModule,
     UserModule,
     ArticleModule,
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
