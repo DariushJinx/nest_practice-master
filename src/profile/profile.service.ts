e273cbacade95c82f61c520ee72c896cf48b3c47
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-10-06 21:07:02 +0330

    handle service for profile

---
+++ src/profile/profile.service.ts
@@ -0,0 +1,105 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
+import { ProfileType } from './types/profile.types';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+import { FollowEntity } from './follow.entity';
+
+export class ProfileService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
+  ) {}
+
+  async getProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: {
+        followerId: currentUserId,
+        followingId: user.id,
+      },
+    });
+
+    return { ...user, following: Boolean(follow) };
+  }
+
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'follower and following cant be equal',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
+  }
+
+  async unFollowProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'follower and following cant be equal',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    return { ...user, following: false };
+  }
+
+  async buildProfileResponse(
+    profile: ProfileType,
+  ): Promise<ProfileResponseInterface> {
+    delete profile.email;
+    delete profile.password;
+    return { profile };
+  }
+}
